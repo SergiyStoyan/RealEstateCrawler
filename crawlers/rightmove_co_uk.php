1a0fb30fed20e210e0be7b6c66248a8d37a5c923
Sergiy Stoyan <s.y.stoyan@gmail.com>
2024-02-14 14:47:19 +0200

    Update rightmove_co_uk.php

--- crawlers/rightmove_co_uk.php
+++ crawlers/rightmove_co_uk.php
@@ -70,7 +70,7 @@ class rightmove_co_uk extends Crawler6_3_sale
 		
 		$headline =	Downloader::Xpath()->GetAttributeValue('//meta[@property="og:description"]', 'content');
 			
-		$description = Downloader::Xpath()->GetJoinedInnerHtml('//article[./div/h2[contains(text(), "description")]]');
+		$description = Downloader::Xpath()->GetJoinedInnerHtml('//article[./div/h2[contains(text(), "Description")]]');
 			
 		//$address = Downloader::Xpath()->GetJoinedInnerHtml('//div[@itemprop="address"]');
 		$address = Downloader::Xpath()->GetJoinedInnerHtml('//head/title');
